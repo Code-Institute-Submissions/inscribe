299c5e54ef8740c7d6b06d676d93fd53b0fc51ee
LHBank <wmbanksph@gmail.com>
2021-03-20 13:23:04 +0000

    Added route functionality to about/registration/sigin.html files.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -28,6 +29,81 @@ def home():
             "home.html", user=user)
     else:
         return render_template("home.html")
+
+
+# About Route
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# Registration Route
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == 'POST':
+        # Confirms for an Existing Username.
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Sorry, That Username's Been Taken.")
+            return redirect(url_for("registration"))
+
+        password = request.form.get("password")
+        pw_check = request.form.get("confirm-password")
+        username = request.form.get("user")
+
+        if password != pw_check:
+            flash("That Password's Not Right, Try Again.")
+
+        if password == username:
+            flash("Is That the Right Password for This Account?")
+
+        if password == pw_check:
+
+            registration = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(
+                    request.form.get("password"))
+            }
+
+        mongo.db.users.insert_one(registration)
+
+        # Enters the New User into a Session.
+        session["user"] = request.form.get("username").lower()
+        flash("Hey There {}. Ready to Write?".format(
+            request.form.get("username").capitalize()))
+        return redirect(url_for(
+            "profile", username=session["user"]))
+    return render_template("registration.html")
+
+
+# Sign In Route
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        # Confirms for an Existing Username.
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Confirms Password Matches User's.
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                # Confirms Incorrect Password Inputted
+                flash("Sorry, Something's Not Right")
+                return redirect(url_for("signin"))
+
+        else:
+            # Non-Existent Username
+            flash("Sorry, Something's Not Right")
+            return redirect(url_for("signin"))
+
+    return render_template("signin.html")
 
 
 if __name__ == "__main__":
