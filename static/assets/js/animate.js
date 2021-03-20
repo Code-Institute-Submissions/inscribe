7fe0a146aa843e3ac0ecf4a25100e7372d98c045
LHBank <wmbanksph@gmail.com>
2021-03-20 13:13:24 +0000

    Added html content to home.html. Added corresponding CSS to home.html. Added Scrollreveal JS to new js files.

---
+++ static/assets/js/animate.js
@@ -0,0 +1,20 @@
+//Scroll Reveal
+window.sr = ScrollReveal();
+  sr.reveal('.mainHeadingOne',{
+    delay: 0,
+    duration: 3500,
+    origin:'left',
+    distance: '750px'
+  });
+  sr.reveal('.mainHeadingTwo',{
+    delay: 2000,
+    duration: 3500,
+    origin:'left',
+    distance: '750px'
+  });
+  sr.reveal('.mainButton',{
+    delay: 6000,
+    duration: 3500,
+    origin:'top',
+    distance: '750px'
+  });
